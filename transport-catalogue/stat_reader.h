c41d2d3ece5220dac30ab918ce2d159b2af87055
Maestro8701 <149194877+Maestro8701@users.noreply.github.com>
2024-03-09 01:17:02 +0700

    Update stat_reader.h
    sprint 8 b

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -1,1 +1,18 @@
 // место для вашего кода
+#pragma once
+
+#include <iosfwd>
+#include <string_view>
+
+#include "transport_catalogue.h"
+
+#include <iostream>
+
+namespace transport {
+
+void ParseAndPrintStatBus(const Catalogue& catalogue, std::string_view request,
+                       std::ostream& output);
+void ParseAndPrintStatStop(const Catalogue& catalogue, std::string_view request,
+                       std::ostream& output);
+    
+} // namespace transport
