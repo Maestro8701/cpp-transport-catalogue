f4902ca7119abe8ed744c29aa5a8d6f0eaf04e96
Maestro8701 <149194877+Maestro8701@users.noreply.github.com>
2024-06-02 03:40:07 +0700

    Create json_reader.h

---
+++ transport-catalogue/json_reader.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <vector>
+#include <string>
+#include <map>
+#include <utility>
+#include "json.h"
+#include "transport_catalogue.h"
+#include "request_handler.h"
+#include "map_renderer.h"
+#include <sstream>
+
+/*
+ * Здесь можно разместить код наполнения транспортного справочника данными из JSON,
+ * а также код обработки запросов к базе и формирование массива ответов в формате JSON
+ */
+
+using namespace TC;
+class JsonReader {
+    private:
+    json::Document document_;
+    
+    void AddStops(const json::Array& base_requests, TransportCatalogue& catalogue);
+    void AddRouteDistances(const json::Array& base_requests, TransportCatalogue& catalogue);
+    void AddBuses(const json::Array& base_requests, TransportCatalogue& catalogue);
+    void ApplyBaseCommands(const json::Array& base_requests, TransportCatalogue& catalogue);
+    void ApplyGetBuses(RequestHandler& request_handler, json::Dict& response, const json::Dict& request);
+    void ApplyGetStops(RequestHandler& request_handler, json::Dict& response, const json::Dict& request);
+    json::Node ApplyStatCommands(const json::Array& stat_requests, RequestHandler& request_handler);
+    void ApplyGetMap(RequestHandler& request_handler, json::Dict& response);
+    svg::Color SetColor(const json::Node& render_setting);
+        
+    public:
+    JsonReader(json::Document document)
+    : document_(std::move(document)){
+    }
+    
+    RenderSettings GetRenderSettings();
+    
+    json::Document ApplyCommands(RequestHandler& request_handler, TransportCatalogue& catalogue);
+};
