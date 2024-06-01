d14a4d529edae2e6dbf0f8ffb7e6381000b163a7
Maestro8701 <149194877+Maestro8701@users.noreply.github.com>
2024-06-02 03:41:20 +0700

    Update main.cpp
    sprint10

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,15 +1,23 @@
+#include <cstdio>
 
-#include <iostream>
-#include <string>
-
-#include "input_reader.h"
-#include "stat_reader.h"
-
-using namespace std;
+#include "json_reader.h"
+#include "request_handler.h"
+#include "transport_catalogue.h"
 
 int main() {
-    transport_catalogue::TransportCatalogue catalogue;
-
-    reader::input::Read(catalogue, cin);
-    reader::stat::Read(catalogue, cin, cout);
+    /*
+     * Примерная структура программы:
+     *
+     * Считать JSON из stdin
+     * Построить на его основе JSON базу данных транспортного справочника
+     * Выполнить запросы к справочнику, находящиеся в массива "stat_requests", построив JSON-массив
+     * с ответами Вывести в stdout ответы в виде JSON
+     */
+    TC::TransportCatalogue catalogue;
+    MapRenderer renderer;
+    RequestHandler request_handler(catalogue, renderer);
+    JsonReader json_reader(json::Load(std::cin));
+    renderer.SetSettings(json_reader.GetRenderSettings());
+    renderer.CreateDocument(request_handler.GetRoutes(), request_handler.GetStops(), request_handler.GetStopToBuses());
+    json::Print(json_reader.ApplyCommands(request_handler, catalogue), std::cout);
 }
