50379f917cfb57dc82b3ca624cdd206ec9341018
Maestro8701 <149194877+Maestro8701@users.noreply.github.com>
2024-07-15 19:55:35 +0700

    Create domain.h

---
+++ transport-catalogue/domain.h
@@ -0,0 +1,52 @@
+// Вставьте сюда решение из предыдущего спринта
+#pragma once
+
+#include <optional>
+#include <string>
+#include <vector>
+
+#include "geo.h"
+
+enum class EdgeType {
+    TRAVEL,
+    WAIT,
+};
+
+struct Stop {
+    std::string stop_name;
+    geo::Coordinates coordinates;
+};
+
+using TypeRoute = std::pair<bool, const Stop*>;
+
+struct Bus {
+    Bus(std::string& bus_name);
+    std::string bus_name;
+    std::vector<Stop*> stops;
+    TypeRoute is_roundtrip;
+};
+
+struct BusStat {
+    size_t stops;
+    size_t unique_stops;
+    double length;
+    double curvature;
+};
+
+struct Distance {
+    Distance() = default;
+    Distance(int value);
+    int meters;
+};
+
+struct RoutingSettings {
+    RoutingSettings() = default;
+    RoutingSettings(double bus_wait_time, double bus_velocity);
+
+    double bus_wait_time = 0.0;
+    double bus_velocity  = 0.0;
+};
+
+struct StopPairHasher {
+    std::size_t operator()(const std::pair<const Stop*, const Stop*>& pair) const;
+};
