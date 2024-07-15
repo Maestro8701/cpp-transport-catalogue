1c2135764e715000120052c9476216820f38c780
Maestro8701 <149194877+Maestro8701@users.noreply.github.com>
2024-07-15 19:59:43 +0700

    Update json_reader.h

--- transport-catalogue/json_reader.h
+++ transport-catalogue/json_reader.h
@@ -1,42 +1,39 @@
+// Вставьте сюда решение из предыдущего спринта
 #pragma once
 
-#include <vector>
-#include <string>
-#include <map>
-#include <utility>
-#include "json.h"
-#include "transport_catalogue.h"
-#include "request_handler.h"
-#include "map_renderer.h"
+#include <memory>
 #include <sstream>
+
 #include "json_builder.h"
+#include "request_handler.h"
 
-/*
- * Здесь можно разместить код наполнения транспортного справочника данными из JSON,
- * а также код обработки запросов к базе и формирование массива ответов в формате JSON
- */
+namespace json {
 
-using namespace TC;
-class JsonReader {
-    private:
-    json::Document document_;
-    
-    void AddStops(const json::Array& base_requests, TransportCatalogue& catalogue);
-    void AddRouteDistances(const json::Array& base_requests, TransportCatalogue& catalogue);
-    void AddBuses(const json::Array& base_requests, TransportCatalogue& catalogue);
-    void ApplyBaseCommands(const json::Array& base_requests, TransportCatalogue& catalogue);
-    void ApplyGetBuses(RequestHandler& request_handler, json::Builder& builder, const json::Dict& request);
-    void ApplyGetStops(RequestHandler& request_handler, json::Builder& builder, const json::Dict& request);
-    json::Node ApplyStatCommands(const json::Array& stat_requests, RequestHandler& request_handler);
-    void ApplyGetMap(RequestHandler& request_handler, json::Builder& builder);
-    svg::Color SetColor(const json::Node& render_setting);
-        
+    class JsonReader {
+
     public:
-    JsonReader(json::Document document)
-    : document_(std::move(document)){
-    }
-    
-    RenderSettings GetRenderSettings();
-    
-    json::Document ApplyCommands(RequestHandler& request_handler, TransportCatalogue& catalogue);
-};
+        JsonReader() = default;
+
+        void LoadHandler(RequestHandler handler);
+        void LoadJson(std::istream& input);
+        const Array& GetBaseRequests()  const;
+        const Array& GetStatRequests()  const;
+        const Dict& GetRenderSetting()  const;
+        const Dict& GetRoutingSetting() const;
+        void AddStopsDataToCatalogue()  const;
+        void AddBusesDataToCatalogue()  const;
+        void AddRoutingSetting()        const;
+        svg::Color HandlingColor(const Node& value) const;
+        void ParseRenderSettings(renderer::MapRenderer& renderer) const;
+        Node GetStatForBusRequest(const std::string_view name, int request_id);
+        Node GetStatForStopRequest(const std::string_view name, int request_id);
+        Node GetMapScheme(RequestHandler& handler, int request_id);
+        Node GetRouteInfo(const std::string_view from, const std::string_view to, int request_id);
+        void ParseAndPrintStat(RequestHandler& handler, std::ostream& out);
+
+    private:
+        std::unique_ptr<Document> document_;
+        std::unique_ptr<RequestHandler> handler_;
+    };
+
+} //namespace json
