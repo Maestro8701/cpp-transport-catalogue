980f9238ec3f5248a8eba12ca49b839e9c0d15b6
Maestro8701 <149194877+Maestro8701@users.noreply.github.com>
2024-08-09 15:21:22 +0700

    Update transport_router.h

--- transport-catalogue/transport_router.h
+++ transport-catalogue/transport_router.h
@@ -1,56 +1,56 @@
-#pragma once
+#pragma once 
+ 
+#include "router.h" 
+#include "transport_catalogue.h" 
+ 
+#include <memory> 
+ 
+namespace transport_router { 
+ 
+	constexpr static double METERS_PER_KM = 1000.0; 
+	constexpr static double MIN_PER_HOUR  = 60.0; 
+ 
+	struct RouteItem { 
+		std::string edge_name; 
+		int span_count = 0; 
+		double time = 0.0; 
+		EdgeType type; 
+	}; 
+ 
+	struct RouteData { 
+		double total_time = 0.0; 
+		std::vector<RouteItem> items; 
+		bool founded = false; 
+	}; 
+ 
+	struct VertexWithMirror { 
+		size_t wait; 
+		size_t travel; 
+	}; 
+ 
+class TransportRouter {
+    using Router   = graph::Router<double>;
+    using Graph    = graph::DirectedWeightedGraph<double>;
+    using Vertexes = std::unordered_map<std::string_view, VertexWithMirror>;
+    using EdgesInfo = std::unordered_map<graph::EdgeId, RouteItem>;
 
-#include "router.h"
-#include "transport_catalogue.h"
+public:
+    explicit TransportRouter(const RoutingSettings& settings, const TransportCatalogue& db);
 
-#include <memory>
+    RoutingSettings GetRoutingSettings(); 
+    RouteData CalculateRoute(std::string_view from, std::string_view to); 
 
-namespace transport_router {
+private:
+    RoutingSettings settings_; 
+    Graph graph_; 
+    std::unique_ptr<Router> router_ = nullptr; 
+    const TransportCatalogue& tc_; 
+    Vertexes vertexes_; 
+    EdgesInfo edges_info_; 
 
-	constexpr static double METERS_PER_KM = 1000.0;
-	constexpr static double MIN_PER_HOUR  = 60.0;
-
-	struct RouteItem {
-		std::string edge_name;
-		int span_count = 0;
-		double time = 0.0;
-		EdgeType type;
-	};
-
-	struct RouteData {
-		double total_time = 0.0;
-		std::vector<RouteItem> items;
-		bool founded = false;
-	};
-
-	struct VertexWithMirror {
-		size_t wait;
-		size_t travel;
-	};
-
-	class TransportRouter {
-
-		using Router	= graph::Router<double>;
-		using Graph		= graph::DirectedWeightedGraph<double>;
-		using Vertexes  = std::unordered_map<std::string_view, VertexWithMirror>;
-		using EdgesInfo = std::unordered_map<graph::EdgeId, RouteItem>;
-
-	public:
-		explicit TransportRouter(const TransportCatalogue& db);
-
-		void SetRoutingSettings(const RoutingSettings& settings);
-		RoutingSettings GetRoutingSettings();
-		RouteData CalculateRoute(std::string_view from, std::string_view to);
-
-	private:
-		RoutingSettings settings_;
-		Graph graph_;
-		std::unique_ptr<Router> router_ = nullptr;
-		const TransportCatalogue& tc_;
-		Vertexes vertexes_;
-		EdgesInfo edges_info_;
-
-		void BuildGraph();
-	};
-
-} // namespace transport_router
+    void BuildGraph();
+    void AddStopsToGraph(size_t& vertex_id);  // ADDITIONAL DECLARATION
+    void AddBusesToGraph(double velocity_factor); // ADDITIONAL DECLARATION
+};
+ 
+} // namespace transport_router 
