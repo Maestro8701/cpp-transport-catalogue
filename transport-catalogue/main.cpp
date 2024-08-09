0e8f615afec2d3a993eead4efe77c268a7e1d11b
Maestro8701 <149194877+Maestro8701@users.noreply.github.com>
2024-08-09 15:23:11 +0700

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -11,17 +11,18 @@ using namespace json;
 int main() {
     TransportCatalogue catalogue;
     renderer::MapRenderer renderer;
-    transport_router::TransportRouter router(catalogue);
-    RequestHandler handler(catalogue, renderer, router);
     JsonReader reader;
-    reader.LoadHandler(handler);
+
 
     istream& input = cin;
     reader.LoadJson(input);
 
+        transport_router::TransportRouter router(reader.AddRoutingSetting(), catalogue);
+    RequestHandler handler(catalogue, renderer, router);
+    reader.LoadHandler(handler);
     reader.AddStopsDataToCatalogue();
     reader.AddBusesDataToCatalogue();
-    reader.AddRoutingSetting();
+    //reader.AddRoutingSetting();
     reader.ParseRenderSettings(renderer);
 
     ostream& out = cout;
